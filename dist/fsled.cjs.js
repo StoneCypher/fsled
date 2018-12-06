152ec9793781b489f9dd9061b464bd373c3ce636
John Haugeland <stonecypher@gmail.com>
2018-12-05 17:42:22 -0800

    rollup progress

---
+++ dist/fsled.cjs.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+let Handle = { Host: undefined };
+function bootstrap(cfg) {
+    Handle.Host = cfg.Host;
+    return Handle;
+}
+
+exports.bootstrap = bootstrap;
+//# sourceMappingURL=fsled.cjs.js.map
